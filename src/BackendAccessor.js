7776f3a742d9665d957703028fe2ea2dd8eb8eb5
Nour Elabbasy <nour@Nours-MacBook-Pro.local>
2020-01-16 16:12:46 -0500

    add file

--- src/BackendAccessor.js
+++ src/BackendAccessor.js
@@ -6,4 +6,15 @@ export function getData(endpoint) {
 
     return fetch(`${url}/${endpoint}`,
                 {"method": "GET", "mode": "cors"});
+
 }
+  export function postData(endpoint, body) {
+      const url = process.env.NODE_ENV === "development" ?
+          "http://localhost:5000" : "http://coa-flask-app-prod.us-east-1.elasticbeanstalk.com";
+
+      console.log(`postData url=${url}/${endpoint}`);
+
+      return fetch(`${url}/${endpoint}`,
+                  {"method": "POST", "mode": "cors",body:JSON.stringify(body)});
+
+  }
